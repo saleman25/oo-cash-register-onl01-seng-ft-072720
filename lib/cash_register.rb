31789648bbcce50b403bb097fa688e82e36aea51
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-08 01:39:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+class CashRegister 
+  
+attr_accessor  :total , :discount, :items, :last_transactions_amount
 
+  
+def initialize(discount = 0)
+  @total = 0
+  @discount = discount
+  @items = []
+end  
+  
+def total 
+  @total
+end 
+
+def add_item(title, price, quantity = 1) 
+   
+end 
+
+def apply_discount 
+end 
+
+def items
+  
+end 
+
+def void_last_transaction
+@total -= @last_transactions_amount
+end 
+  
+  
+end 
